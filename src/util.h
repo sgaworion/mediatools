6232bf7534f90dad4dccc1efc258a415735b1258
sgaworion <123153505+sgaworion@users.noreply.github.com>
2023-07-29 17:06:10 -0500

    Delete src directory

--- src/util.h
+++
@@ -1,7 +0,0 @@
-#ifndef _UTIL_H_DEFINED
-#define _UTIL_H_DEFINED
-
-#include <libavformat/avformat.h>
-int open_input_correct_demuxer(AVFormatContext **ctx, const char *filename);
-
-#endif
--- src/validation.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VALIDATION_H_DEFINED
-#define _VALIDATION_H_DEFINED
-
-#include <libavformat/avformat.h>
-
-int mediatools_validate_video(AVFormatContext *format);
-int mediatools_validate_duration(AVRational dur);
-
-#endif
